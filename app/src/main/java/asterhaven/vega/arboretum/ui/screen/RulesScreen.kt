ccc90cccd5526016402889d3ff4a025c7b88ffcd
claritywhisk <98000487+claritywhisk@users.noreply.github.com>
2024-04-25 21:54:58 -0400

    cursor UI for editing rules

--- app/src/main/java/asterhaven/vega/arboretum/ui/screen/RulesScreen.kt
+++ app/src/main/java/asterhaven/vega/arboretum/ui/screen/RulesScreen.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
@@ -38,6 +42,7 @@ import asterhaven.vega.arboretum.R
 import asterhaven.vega.arboretum.lsystems.LWord
 import asterhaven.vega.arboretum.lsystems.Specification
 import asterhaven.vega.arboretum.ui.components.LabeledSection
+import java.lang.StringBuilder
 
 private const val NOT_EDITING = -1
 private const val EDITING_AXIOM = Int.MAX_VALUE
@@ -49,19 +54,49 @@ fun RulesScreen(
     val axiom by remember { mutableStateOf(baseSpecification.initialRaw) }
     val productionRules =
         remember { mutableStateListOf(*baseSpecification.productionsRaw.toTypedArray()) }
+    val editingCursorPos = remember { mutableStateOf(NOT_EDITING) }
     val editingRow = remember { mutableStateOf(NOT_EDITING) }
-    val cursorPos = remember { mutableStateOf(NOT_EDITING) }
+    val editingString = remember { mutableStateOf<StringBuilder?>(null) }
     var reorderDeleteButtonsVisible by remember { mutableStateOf(false) }
     @Composable
-    fun AccursedText(text: String, modifier: Modifier = Modifier) {
-        Text(buildAnnotatedString {
-            append(text)
-            if (cursorPos.value != NOT_EDITING)
-                addStyle(SpanStyle(background = Color.Cyan), cursorPos.value, cursorPos.value + 1)
-        }, modifier)
+    fun AccursedText(text: String, thisRow : Int, modifier: Modifier = Modifier) {
+        val editingThis = remember { mutableStateOf(false) }
+        ClickableText(
+            modifier = modifier.padding(8.dp),
+            text = buildAnnotatedString {
+                //Deliver the text and cursor/highlight TODO
+                append(text)
+                if (editingCursorPos.value != NOT_EDITING)
+                    addStyle(SpanStyle(background = Color.Cyan), editingCursorPos.value, editingCursorPos.value + 1)
+            }
+        ) { clickedCharOffset ->
+            if(!editingThis.value) {
+                editingRow.value = thisRow
+                editingString.value = StringBuilder(text)
+                editingThis.value = true
+            } // proceed to offset even when first starting editing
+            editingCursorPos.value = 1 + clickedCharOffset
+        }
     }
     @Composable
     fun EditRow() {
+        Row(Modifier.fillMaxWidth()) {
+            IconButton(enabled = editingCursorPos.value > 0, onClick = {
+                editingCursorPos.value--
+            }) {
+                Icon(Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = "Move cursor left")
+            }
+            IconButton(enabled = editingCursorPos.value < editingString.toString().length, onClick = {
+                editingCursorPos.value++
+            }) {
+                Icon(Icons.AutoMirrored.Filled.KeyboardArrowRight, contentDescription = "Move cursor right")
+            }
+            IconButton(enabled = editingCursorPos.value > 0, onClick = {
+
+            }) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Backspace")
+            }
+        }
         Row(Modifier.fillMaxWidth()) {
             DropdownMenu(expanded = true, onDismissRequest = { editingRow.value = NOT_EDITING }) {
                 LWord.standardSymbols.forEach {
@@ -100,17 +135,17 @@ fun RulesScreen(
     ) {
         LabeledSection(LocalContext.current.getString(R.string.rules_label_axiom)) {
             Row(Modifier.fillMaxWidth()) {
-                AccursedText(axiom)
+                AccursedText(axiom, EDITING_AXIOM)
             }
             if (editingRow.value == EDITING_AXIOM) EditRow()
         }
         LabeledSection(LocalContext.current.getString(R.string.rules_label_productions)) {
             productionRules.forEachIndexed { iRule, pr ->
                 Row(Modifier.fillMaxWidth()) {
-                    AccursedText(pr.before)
-                    Icon(Icons.Default.ArrowForward, contentDescription = "Arrow")
+                    AccursedText(pr.before, iRule)
+                    Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "Arrow")
                     AccursedText(
-                        pr.after, Modifier
+                        pr.after, iRule, Modifier
                             .weight(1f)
                             .width(IntrinsicSize.Max)
                     )
