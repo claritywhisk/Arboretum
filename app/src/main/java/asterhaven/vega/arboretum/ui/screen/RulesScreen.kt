8c72344d1f0f3d69c2103194f884c87db2cc1b74
claritywhisk <98000487+claritywhisk@users.noreply.github.com>
2024-04-30 09:42:51 -0400

    todos wip

--- app/src/main/java/asterhaven/vega/arboretum/ui/screen/RulesScreen.kt
+++ app/src/main/java/asterhaven/vega/arboretum/ui/screen/RulesScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
@@ -46,10 +45,10 @@ import asterhaven.vega.arboretum.R
 import asterhaven.vega.arboretum.lsystems.LWord
 import asterhaven.vega.arboretum.lsystems.Specification
 import asterhaven.vega.arboretum.ui.components.LabeledSection
-import java.lang.StringBuilder
 
 private const val NOT_EDITING = -1
 private const val EDITING_AXIOM = Int.MAX_VALUE
+private const val EMPTY_STRING = ""
 
 @Composable
 fun RulesScreen(
@@ -60,7 +59,7 @@ fun RulesScreen(
         remember { mutableStateListOf(*baseSpecification.productionsRaw.toTypedArray()) }
     val editingCursorPos = remember { mutableStateOf(NOT_EDITING) }
     val editingRow = remember { mutableStateOf(NOT_EDITING) }
-    val editingString = remember { mutableStateOf<StringBuilder?>(null) }
+    val editingString = remember { mutableStateOf(EMPTY_STRING) }
     var reorderDeleteButtonsVisible by remember { mutableStateOf(false) }
 
     fun Modifier.consumeClickEventsWhen(predicate : () -> Boolean) = this.pointerInput(Unit) {
@@ -72,6 +71,14 @@ fun RulesScreen(
         }
     }
     fun Modifier.consumeClickEvents() = this.consumeClickEventsWhen { true }
+    fun rangeOfCursorIndexOrWord() : IntRange {
+        val s = editingString.value
+        val c = editingCursorPos.value
+        return when(s[c]) {
+            '(' -> c - 1..(c..s.lastIndex).first { s[it] == ')' }
+            else -> c..c
+        }
+    }
     @Composable
     fun AccursedText(text: String, thisRow : Int, modifier: Modifier = Modifier) {
         val editingThis = remember { mutableStateOf(false) }
@@ -82,40 +89,56 @@ fun RulesScreen(
             modifier = modifier.padding(8.dp),
             style = TextStyle(color = MaterialTheme.colorScheme.onBackground),
             text = buildAnnotatedString {
-                //Deliver the text and cursor/highlight TODO
+                //Deliver the text and cursor/highlight
                 append(text)
-                if(editingThis.value) addStyle(
-                    SpanStyle(background = Color.Cyan),
-                    editingCursorPos.value,
-                    editingCursorPos.value + 1
-                )
+                if(editingThis.value) {
+                    val sStyle = SpanStyle(background = MaterialTheme.colorScheme.tertiary)
+                    if (text[editingCursorPos.value] == '(') {
+                        addStyle(sStyle, editingCursorPos.value - 1, editingCursorPos.value + 1)
+                        val iRightParen = (editingCursorPos.value..text.lastIndex)
+                            .first { text[it] == ')' }
+                        addStyle(sStyle, iRightParen, iRightParen + 1)
+                    } else addStyle(sStyle, editingCursorPos.value, editingCursorPos.value + 1)
+                }
             }
         ) { clickedCharOffset ->
-            //todo draft
-            editingCursorPos.value = 1 + clickedCharOffset
+            val nextI = clickedCharOffset + 1
+            editingCursorPos.value = when {
+                nextI <= text.lastIndex && text[nextI] == '(' -> nextI
+                text[clickedCharOffset] == ',' -> clickedCharOffset - 1
+                text[clickedCharOffset] == ')' -> clickedCharOffset - 1
+                else -> clickedCharOffset
+            }
             editingRow.value = thisRow
-            editingString.value = StringBuilder(text)
+            editingString.value = text
             editingThis.value = true
         }
     }
     @Composable
     fun EditRow() {
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .consumeClickEvents()) {
+        Row(Modifier.consumeClickEvents()) {
+            fun charAtCursor(offset : Int) = editingString.value[editingCursorPos.value + offset]
             IconButton(enabled = editingCursorPos.value > 0, onClick = {
-                editingCursorPos.value -= 1
+                editingCursorPos.value -= when {
+                    charAtCursor( 0) == '(' -> 2
+                    charAtCursor(-1) == ',' -> 2
+                    else -> 1
+                }
             }) {
                 Icon(Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = "Move cursor left")
             }
-            IconButton(enabled = editingCursorPos.value < editingString.toString().length, onClick = {
-                editingCursorPos.value += 1
+            IconButton(enabled = editingCursorPos.value < editingString.value.length, onClick = {
+                editingCursorPos.value += when {
+                    charAtCursor(2) == '(' -> 2
+                    charAtCursor(1) == ',' -> 2
+                    charAtCursor(1) == ')' -> 2
+                    else -> 1
+                }
             }) {
                 Icon(Icons.AutoMirrored.Filled.KeyboardArrowRight, contentDescription = "Move cursor right")
             }
             IconButton(enabled = editingCursorPos.value > 0, onClick = {
-                //TODO
+                editingString.value = editingString.value.removeRange(rangeOfCursorIndexOrWord())
             }) {
                 Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Backspace")
             }
@@ -131,7 +154,16 @@ fun RulesScreen(
                             }
                         },
                         onClick = {
-                            //TODO
+                            val parameters = if(it.params == 0) "" else {
+                                StringBuilder().apply {
+                                    append("(")
+                                    repeat(it.params - 1) { append(" ,") }
+                                    append(" )")
+                                }.toString()
+                            }
+                            val s = " " + it.symbol + parameters + " "
+                            editingString.value.replaceRange(rangeOfCursorIndexOrWord(), s)
+                            editingCursorPos.value += if(it.params == 0) 2 else 3
                         }
                     )
                 }
@@ -169,7 +201,7 @@ fun RulesScreen(
                         if (event.changes.none { it.isConsumed }) {
                             editingCursorPos.value = NOT_EDITING
                             editingRow.value = NOT_EDITING
-                            editingString.value = null
+                            editingString.value = EMPTY_STRING
                         }
                     }
                 }
@@ -186,10 +218,10 @@ fun RulesScreen(
         }
         LabeledSection(LocalContext.current.getString(R.string.rules_label_productions)) {
             productionRules.forEachIndexed { iRule, pr ->
-                Row(
-                    Modifier
-                        .fillMaxWidth()
-                        .consumeClickEventsWhen { editingRow.value == iRule }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .consumeClickEventsWhen { editingRow.value == iRule },
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
                     AccursedText(pr.before, iRule)
                     Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "Arrow")
